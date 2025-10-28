6b3f105c5fef7ec84bcb9e0d2ad4dcd3710acecf
vientorepublic <markgod0419@gmail.com>
2025-10-28 17:19:11 +0900

    feat: add standardized error codes for validation, database, file upload, OAuth2, authentication, and general errors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 // Permissions and RBAC related exports
 export * from "./constants.js";
 export * from "./utils.js";
+
+// Error codes shared between frontend and backend
+export * from "./error-codes.js";
